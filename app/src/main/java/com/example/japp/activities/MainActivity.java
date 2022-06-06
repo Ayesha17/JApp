984949f3b61ab257938fdc56935d9fd0da099179
Ayesha <ayesha@nextgeni.dev>
2022-06-06 17:19:08 +0500

    add firebase crashlytics

--- app/src/main/java/com/example/japp/activities/MainActivity.java
+++ app/src/main/java/com/example/japp/activities/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.japp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.japp.R;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -15,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseAnalytics.getInstance(getApplicationContext());
+        Button crashButton = new Button(this);
+        crashButton.setText("Test Crash");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                throw new RuntimeException("Test Crash"); // Force a crash
+            }
+        });
+
+        addContentView(crashButton, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 }
